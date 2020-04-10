fea588163c4acb561003141168e5a2d677f7f015
Sergeyakzhik <sergeyakzhik@gmail.com>
2020-04-10 03:18:20 -0500

    Updates

---
+++ precache-manifest.b2d2f80472dabb8cd7d557535572bff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55209e43c5debad4690d11a809a89630",
+    "url": "/duden-app/index.html"
+  },
+  {
+    "revision": "4e7b4e5b817f0d40b125",
+    "url": "/duden-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c01760e91bc7f1146ff9",
+    "url": "/duden-app/static/css/main.84960ab6.chunk.css"
+  },
+  {
+    "revision": "4e7b4e5b817f0d40b125",
+    "url": "/duden-app/static/js/2.3db6b4c3.chunk.js"
+  },
+  {
+    "revision": "b3b39afb7fd85786e789e297927f435e",
+    "url": "/duden-app/static/js/2.3db6b4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01760e91bc7f1146ff9",
+    "url": "/duden-app/static/js/main.52c3cec4.chunk.js"
+  },
+  {
+    "revision": "c81a96517ccf43e2a074",
+    "url": "/duden-app/static/js/runtime-main.8b8b14ff.js"
+  }
+]);
